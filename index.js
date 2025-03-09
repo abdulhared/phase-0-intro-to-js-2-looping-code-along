08588ebd6e18915f8a932d54cda7b2bf2e70c713
abdulhared <abdulhared632@gmail.com>
2025-03-09 22:42:15 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+
+function writeCards(names=["Guadalupe", "Ollie", "Aki"],event= "surprise"){
+    return names.map(name =>  Thank you, ${name}, for the wonderful ${event} gift!);
+  }
+  function countDown(number= 10) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+     }
+   }
